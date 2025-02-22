29483c9e417ce9a128b9403e4877cd0bdbce77ad
Lang Towl <langtowl@gmail.com>
2025-02-22 12:16:36 -0500

    Updated the POST method to do some stuff

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+dummy = {
+    "header": "Authenticate",
+    "fname": "John",
+    "lname": "Smith",
+    "pword": "pword123"
+}
+
+dummy_response = {
+    "status": int,
+    "message": str
+}
 
 @app.route("/", methods=["GET"])
 def get():
@@ -10,7 +22,9 @@ def get():
 
 @app.route("/", methods=["POST"])
 def post():
-    return jsonify({"message": "echo POST"})
+    data = request.get_json()
+
+    return jsonify({"status": 200, "message": data["fName"]}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
