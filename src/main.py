fc0c9d80231b98704a168b8f12f9212bfcb8d59c
Lang Towl <langtowl@gmail.com>
2025-03-23 14:14:49 -0400

    Now handles map requests and updated CORS policy

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 CORS(app,
      origins=["http://localhost:4200"],
      supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization", "ngrok-skip-browser-warning"]
+     allow_headers=["Content-Type", "Authorization", "ngrok-skip-browser-warning", "token", "lat", "long"]
      )
 
 @app.route("/test", methods = ["GET"])
@@ -20,13 +20,13 @@ def testPost():
 
 @app.route("/login", methods = ["GET"])
 def getLogin():
-    print("INCOMING LOGIN GET REQUEST RECEIVED -> /")
+    print("INCOMING LOGIN GET REQUEST RECEIVED -> /login")
     return jsonify({"response": "Hello GET Server"}), 200
 
 @app.route("/login", methods = ["POST"])
 @app.route("/create-account", methods = ["POST"])
 def postLogin():
-    print("INCOMING LOGIN POST REQUEST RECEIVED -> /")
+    print("INCOMING LOGIN POST REQUEST RECEIVED -> /create-account or /login")
 
     good_payload = {
         "ok": True,
@@ -49,7 +49,7 @@ def postLogin():
 
 @app.route("/validate-token", methods = ["POST"])
 def validateToken():
-    print("INCOMING TOKEN VALIDATION POST REQUEST RECEIVED -> /")
+    print("INCOMING TOKEN VALIDATION POST REQUEST RECEIVED -> /validate-token")
 
     payload = {
         "valid": True,
@@ -58,8 +58,39 @@ def validateToken():
 
     return jsonify(payload), 200
 
+@app.route("/map-nodes", methods = ["GET"])
+def getMapNodes():
+    print("INCOMING GET REQUEST RECEIVED -> /map-nodes")
+
+    payload = {
+        "nodes": {
+            1: {
+                "x": 1,
+                "y": 2,
+                "name": "name",
+                "group": "group",
+                "kind": 0,
+                "tags": [
+                    "tag1"
+                ]
+            },
+            2: {
+                "x": 1,
+                "y": 2,
+                "name": "name",
+                "group": "group",
+                "kind": 0,
+                "tags": [
+                    "tag1"
+                ]
+            }
+        }
+    }
+
+    return jsonify(payload), 200
+
 if __name__ == "__main__":
     print("Starting server...\n")
 
-    app.run(debug = True)
+    app.run(debug = False)
     # app.run(debug = True, ssl_context = ('server.crt', 'server.key'))
