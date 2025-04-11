0c7272398e5cba89d5c81afab291d9b2be3ef2e9
exdisj <exdisj@outlook.com>
2025-04-11 12:28:59 -0400

    Changed DB to use the create_table.sql file for create script. Added a dependencies file

--- src/db.py
+++ src/db.py
@@ -27,57 +27,17 @@ def ensure_tables(cur: sqlite3.Cursor) -> bool:
     Runs the create table scripts, if the tables do not exist.
     """
     try:
-        cur.executescript("""
-                    CREATE TABLE IF NOT EXISTS USERS (
-                        U_ID INTEGER PRIMARY KEY,
-                        USERNAME TEXT NOT NULL, 
-                        F_NAME TEXT NOT NULL,
-                        L_NAME TEXT NOT NULL,
-                        PASSWD BLOB NOT NULL,
-                        SALT BLOB NOT NULL,
-                        CONSTRAINT NAMES UNIQUE (F_NAME, L_NAME)
-                    );
+        contents = str()
+        with open("table_create.sql", "r", encoding="utf-8") as file:
+            contents = file.read()
 
-                    CREATE TABLE IF NOT EXISTS USER_SESSIONS (
-                        U_ID INTEGER PRIMARY KEY,
-                        JWT TEXT NOT NULL,
-                        AT_TIME INTEGER NOT NULL,
-                        
-                        FOREIGN KEY (U_ID) REFERENCES USERS (U_ID) on delete cascade on update cascade
-                    );
-
-                    CREATE TABLE IF NOT EXISTS NODES (
-                        N_ID INTEGER PRIMARY KEY,
-                        X REAL NOT NULL,
-                        Y REAL NOT NULL,
-                        NODE_NAME TEXT,
-                        NODE_GROUP TEXT DEFAULT NULL,
-                        IS_PATH INTEGER
-                        
-                        CONSTRAINT NODE_KIND CHECK (IS_PATH IN (0, 1))
-                    );
-
-                    CREATE TABLE IF NOT EXISTS NODE_TAGS (
-                        TAG_ID INTEGER PRIMARY KEY,
-                        N_ID INTEGER NOT NULL,
-                        TAG TEXT NOT NULL,
-                        
-                        FOREIGN KEY (N_ID) REFERENCES NODES (N_ID) ON DELETE CASCADE ON UPDATE CASCADE
-                        CONSTRAINT ONE_TAG UNIQUE (N_ID, TAG)
-                    );
-
-                    CREATE TABLE IF NOT EXISTS EDGES (
-                        SOURCE INTEGER NOT NULL,
-                        DESTINATION INTEGER NOT NULL,
-
-                        CONSTRAINT PK1 PRIMARY KEY (SOURCE, DESTINATION),
-
-                        FOREIGN KEY (SOURCE) REFERENCES NODES (N_ID) ON DELETE CASCADE ON UPDATE CASCADE,
-                        FOREIGN KEY (DESTINATION) REFERENCES NODES (N_ID) ON DELETE CASCADE ON UPDATE CASCADE
-                    );
-                    """)
+        cur.executescript(contents)
 
         return True
     except sqlite3.Error as e:
         print(f"Unable to create tables '{e}'")
         return False
+ 
+if __name__ == "__main__":
+    # Attempt to create the database
+    connection = open_db("test-db.sqlite")
